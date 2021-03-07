c1344ac6889e3e47d6e496d6fbad6c16bb04bcae
supsun-sockol <55758846+supsun-sockol@users.noreply.github.com>
2021-03-07 19:46:11 +0300

    Usable version

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 # include<iostream>
-#include<fstream>
+#include <fstream>
 #include <unordered_map>
-
-int main(){
-    //ifstream fin("primes50");
-    int q =1009;
-    std::unordered_map <int, float> v;
-    for (int i=0; i<q; i++){
-        v[i]=0;
-        std::cout<<"\b\b\b\b"<<i*100/q<<"%";
+#include <string>
+using namespace std;
+int main() {
+    ifstream fin("primes1.txt");
+    if (!fin.is_open())
+    {
+        return 1;
     }
-    std::cout<<"created\n000%";
-    for (int i=0; i<q; i++){
-        v[(i*i)%q]=1;
-        std::cout<<"\b\b\b\b"<<i*100/q<<"%";
+    const int start = 1;
+    int q;
+    string str;
+    unordered_map <int, bool> v;
+    for (int j = start; j < 10000; j++)
+    {
+        fin >> q;
+        str = "bin/" + to_string(q);
+        ofstream fout(str);
+        cout << j << "\n";
+        for (int i = 1; i < q; i++) {
+            v[(i * i) % q] = 1;
+            cout << "\b\b\b\b" << (i + 1) * 100 / q << "%";
+        }
+        cout << "\n";
+        for (int i = 1; i < q; i++) {
+            if (v[i])
+            {
+                fout << 1;
+            }
+            else fout << 0;
+        }
+        fout.close();
+        v.clear();
     }
-    for (int i=0; i<q; i++){
-        std::cout << v[i];
-    }
+    
 }
-
